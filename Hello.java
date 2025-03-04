6fdc21ed3967ee287a2a431e193a74f346b6b2ae
Jhonattan Curtarelli <Jhonattan.curtarelli@outlook.com>
2025-03-04 16:37:26 -0300

    feat: adding holaquetal

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
+    public static void holaquetal() {
+        System.out.println("Hola, ¿qué tal?");
+    }
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        holaquetal();
     }
 }
